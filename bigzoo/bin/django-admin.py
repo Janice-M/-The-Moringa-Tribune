fa23b476cba3d2483bcfefe72b6221c213f35ad7
Janice-M <janiceink001@gmail.com>
2019-08-19 18:41:48 +0300

    created the the tribune django project and news django app within the moringa tribune

---
+++ bigzoo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringa/Desktop/python/django/The-Moringa-Tribune/bigzoo/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
